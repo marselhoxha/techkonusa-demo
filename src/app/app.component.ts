57fb92e48060d870e184a614f85fce955fbbdab3
marselhoxha <marsel.hox@gmail.com>
2020-07-23 13:13:58 -0400

    implementing login and register pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { User } from './_models';
+import { UserService, AuthenticationService, LoaderService } from './_services';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentUser: User;
   title = 'techkon-demo';
+  timestamp = '12:00 AM';
+  profileName = 'Profile';
+
+  constructor(
+    public userService: UserService,
+    public router: Router,
+    public authenticationService: AuthenticationService,
+    public loaderService: LoaderService
+  ) {
+  }
+
+  ngOnInit() {
+    // set current User profile to header
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AlertComponent } from './_directives';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { AuthGuard } from './_guards';
+import { fakeBackendProvider, JwtInterceptor, ErrorInterceptor, ApiBaseUrlInterceptor, LoaderInterceptor } from './_helpers';
+import { AlertService, AuthenticationService, UserService, MarketDataService, LoaderService } from './_services';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    MarketDataService,
+    LoaderService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ApiBaseUrlInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
