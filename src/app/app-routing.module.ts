a9d7b344255c47d8ab84996a1cb929ca99243b66
marselhoxha <marsel.hox@gmail.com>
2020-07-23 12:48:27 -0400

    importing components for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Import Components for Routes:
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
